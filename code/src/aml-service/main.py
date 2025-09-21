d214202a98a779e8cab8546614e24468609358d3
dhiraj-inti <intidhiraj3103@gmail.com>
2025-09-21 11:52:14 +0530

    Refactor

--- code/src/aml-service/main.py
+++ code/src/aml-service/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import requests
 
 app = Flask("aml-service")
 port = 5000
@@ -8,21 +9,42 @@ def home():
     return "AML Service is running!"
 
 @app.route('/aml-checks', methods=["POST"])
-def aml_checks():
+def aml_checks(request):
     flag = None
     # Step 1: Receive and parse the request data
+    data = request.get_json()
+    sender, receiver, amount, timestamp = data['sender'], data['receiver'], data['amount'], data["timestamp"]
 
     # Step 2: If flagged for explanation, call the explanation service
     # else call the oracle service to add transaction to the blockchain
-    if flag:
-        message = "<Explanation from explanation service>"
-    else:
-        message = "Added to blockchain successfully"
-
+    try:
+        if flag:
+            message = "<Explanation from explanation service>"
+        else:
+            response = invoke_add_to_blockchain_service(sender, receiver, amount, timestamp)
+            message = "Added to blockchain successfully"
+    except Exception as e:
+        return {
+            "flag": None,
+            "message": "Exception occurred: " + str(e)
+        }, 500
+    
     return {
         "flag": flag,
-        "message": "Transaction processed successfully"
-    }
+        "message": message
+    }, 200
+
+def invoke_add_to_blockchain_service(sender, receiver, amount, timestamp):
+    # Logic to call the oracle service to add transaction to the blockchain
+    response = requests.post("http://localhost:8080/add-transaction", json={
+        "sender":sender,
+        "amount":amount,
+        "receiver":receiver,
+        "timestamp":timestamp
+    })
+    if response.status_code != 200:
+        raise Exception("Error calling oracle service")
+    return response.json()
 
 if __name__ == '__main__':
     app.run(debug=True)
